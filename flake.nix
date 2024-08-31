be39ec46f0fa1184dcc13689038d6a5aefa85c55
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-08-31 11:48:47 -0500

    feat!: add base flake.nix configuration

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+    description = "Desktop configuration";
+
+    # git repos used in the configuration. nixpkgs, home-manager, stuff like that. 
+    inputs = {
+        nixpkgs = {
+            url = "github.com/nixos/nixpkgs/nixos-24.05";
+        };
+    };
+
+    # where the system is configured.
+    outputs = { self, nixpkgs, ...}: 
+        let 
+            lib = nixpkgs.lib;
+        in {
+            nixosConfigurations = {
+                jls_desktop = lib.nixosSystem {
+                    system = "x86_64-linux";
+                    modules [ ./configuration.nix ];
+                };
+            };
+        };
+}
