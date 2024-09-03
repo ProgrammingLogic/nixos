3bf15fffc53fc5b4bc8b0c250694cb1c38f80a1c
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-09-03 13:19:11 -0500

    feat!: use gnome instead of plasma

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -63,9 +63,9 @@
     # Enable the X11 windowing system.
     # You can disable this if you're only using the Wayland session.
 
-    # Enable the KDE Plasma Desktop Environment.
-    services.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6.enable = true;
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
 
     # Configure keymap in X11
     services.xserver.xkb = {
