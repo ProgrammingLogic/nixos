c9497dad7452adb8256ff15d4148bd236bdfdc1b
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-09-08 15:11:47 -0500

    fix: remove root / from path

--- users/default/gnome/gnome.nix
+++ users/default/gnome/gnome.nix
@@ -7,17 +7,30 @@
 
     dconf.settings = {
         # Favorite apps (toolbar on bottom)
-        "org/gnome/shell" = {
-            favorite-apps = [
-                "firefox.desktop"
-                "obsidian.desktop"
-                "steam.desktop"
-                "discord.desktop"
-                "spotify.desktop"
-                "signal-desktop.desktop"
-                "lutris.desktop"
-            ];
-        };
-    };
+            "org/gnome/shell" = {
+                favorite-apps = [
+                    "firefox.desktop"
+                    "obsidian.desktop"
+                    "steam.desktop"
+                    "discord.desktop"
+                    "spotify.desktop"
+                    "signal-desktop.desktop"
+                    "lutris.desktop"
+                ];
+            };
 
+        # Power Settings
+            # Do not turn the screen blank after X time
+            "org/gnome/desktop/session" = {
+                idle-delay = 0;
+            };
+
+            "org/gnome/settings-daemon/plugins/power/" = {
+                # Turn off auto suspend
+                sleep-inactive-ac-type = "nothing";
+
+                # Shutdown system when power button is pressed
+                power-button-action = "interactive";
+            };
+    };
 }
