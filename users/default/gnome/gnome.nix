1884053a0a787198e6f773c117edc2d0c3974a8f
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-09-08 15:30:46 -0500

    feat: set background of machine & power options

--- users/default/gnome/gnome.nix
+++ users/default/gnome/gnome.nix
@@ -32,5 +32,31 @@
                 # Shutdown system when power button is pressed
                 power-button-action = "interactive";
             };
+
+        # Multitasking
+            "org/gnome/nutter" = {
+                edge-tiling = true;
+            };
+
+        # Appearance 
+            # Dark mode
+            "org/gnome/desktop/interface" = {
+                color-scheme = "prefer-dark";
+            };
+
+            # Background 
+            "org/gnome/desktop/screensaver" = {
+                picture-uri = "file:///home/jstiverson/.local/share/backgrounds/orthodox_background.jpg";
+            picture-uri-dark = "file:///home/jstiverson/.local/share/backgrounds/orthodox_background.jpg"
+            };
+
+            "org/gnome/desktop/background" = {
+                picture-options = "scaled";
+            };
+    };
+
+    # Set the computer background
+    home.file.".local/share/background/orthodox_background.jpg" = {
+        source = "../../../resources/orthodox_background.jpg";
     };
 }
